1cc8bc1cd7f849d3da8bcdac85eaf03008d9f0bc
ron-vebbuilders <39553105+ron-vebbuilders@users.noreply.github.com>
2018-08-30 18:19:13 +0530

    test
    test

---
+++ footer.php
@@ -0,0 +1,34 @@
+<footer>
+<div class="container">
+<div class="joine-us">
+<h3>Joine with us</h3>
+<div class="sosial">
+<ul class="clearfix">
+  <li><a href="#"><i class="fa fa-facebook"></i>  Facebook</a></li>
+  <li><a href="#"><i class="fa fa-instagram"></i>  Instagram</a></li>
+</ul>
+</div>
+</div>
+</div>
+</footer>
+<!-- <div class="appointment-book">
+<h4>Book An Appointment</h4>
+<a href="#">Book Now</a>
+<span class="close-app"><i class="fa fa-times"></i></span>
+</div> -->
+  </div>  
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/menu.js"></script>
+  <script src="js/wow.js"></script>
+  <script src="js/custom.js"></script>
+
+<script type="text/javascript">
+  $(".close-app").click(function(event) {
+    $(".appointment-book").fadeOut();
+  });
+</script>
+
+</body>
+</html>
